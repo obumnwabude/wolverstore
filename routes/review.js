99a9ce4ed83d672a4d159384af0a8771556b8e8a
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-04-24 21:45:54 +0100

    correct import error in review routes

--- routes/review.js
+++ routes/review.js
@@ -3,7 +3,7 @@ const reviewCtrl = require('../controllers/review');
 const auth = require('../middleware/auth');
 const userId = require('../middleware/user-id');
 const storeId = require('../middleware/store-id');
-const orderId = require('../middleware/order-id');
+const productId = require('../middleware/product-id');
 
 router.post('/', userId, productId, storeId, auth, reviewCtrl.createReview);
 router.get('/', userId, auth, reviewCtrl.getAllReviews);
